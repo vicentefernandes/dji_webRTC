6bc3f5453a6ef4b12947e35653e69e0e780850cc
vmachado <vmachado@gradiant.org>
2021-05-04 22:59:32 +0000

    First version of dji driver + gstreamer webrtc

---
+++ src/DJIDriver.h
@@ -0,0 +1,108 @@
+#include "../common/dji_linux_helpers.hpp"
+#include <dji_vehicle.hpp>
+#include <dji_advanced_sensing.hpp>
+#include <dji_camera_image.hpp>
+#include <iostream>
+#include <pthread.h>
+#include <thread>
+
+#ifdef OPEN_CV_INSTALLED
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/opencv.hpp"
+using namespace cv;
+#endif
+
+using namespace DJI::OSDK;
+using namespace std;
+
+class DJIDriver
+{
+public:
+  DJIDriver() = delete;
+  DJIDriver(int argc, char** argv)
+    : _linuxEnvironment(argc, argv, true){
+
+    };
+
+  ~DJIDriver()
+  {
+    if (_cam_token == "FPV_CAM")
+    {
+      _vehicle->advancedSensing->stopFPVCameraStream();
+    }
+    if (_cam_token == "MAIN_CAM")
+    {
+      _vehicle->advancedSensing->stopMainCameraStream();
+    }
+  };
+
+  bool setup(const std::string& cam_token)
+  {
+    _vehicle = _linuxEnvironment.getVehicle();
+    const char* acm_dev =
+      _linuxEnvironment.getEnvironment()->getDeviceAcm().c_str();
+    _vehicle->advancedSensing->setAcmDevicePath(acm_dev);
+    _cam_token = cam_token;
+    if (_vehicle == nullptr)
+    {
+      std::cout << "Vehicle not initialized, exiting.\n";
+      return false;
+    }
+    return true;
+  };
+
+  void startCapture(uint timeout_secs = 3)
+  {
+    std::thread(&DJIDriver::startCapture_func, this).detach();
+    std::this_thread::sleep_for(std::chrono::seconds(timeout_secs));
+  }
+
+  void stopCapture(uint timeout_secs = 6)
+  {
+    _capture = false;
+    std::this_thread::sleep_for(std::chrono::seconds(timeout_secs));
+  }
+
+  CameraRGBImage get_next_frame()
+  {
+    const std::lock_guard<std::mutex> lock(_mtx);
+    return _current_img_rgb;
+  };
+
+private:
+  static void prepare_image_cb(CameraRGBImage img, void* p)
+  {
+    CameraRGBImage* obj = reinterpret_cast<CameraRGBImage*>(p);
+    /// const std::lock_guard<std::mutex> lock(_mtx);
+    // TODO LOCK
+    cout << "#### Got image from:\t"
+         << "name" << endl;
+    *obj = std::move(img);
+  };
+
+  void startCapture_func()
+  {
+    _capture = true;
+    if (_cam_token == "FPV_CAM")
+    {
+      auto camResult = _vehicle->advancedSensing->startFPVCameraStream(
+        &prepare_image_cb, (void*)&this->_current_img_rgb);
+    }
+    else if (_cam_token == "MAIN_CAM")
+    {
+      auto camResult = _vehicle->advancedSensing->startMainCameraStream(
+        &prepare_image_cb, (void*)&this->_current_img_rgb);
+    }
+    else
+      std::runtime_error("Invalid Camera token [" + _cam_token + "].");
+    while (_capture)
+      std::this_thread::sleep_for(std::chrono::seconds(1));
+  };
+
+  LinuxSetup     _linuxEnvironment;
+  Vehicle*       _vehicle;
+  std::string    _cam_token;
+  CameraRGBImage _current_img_rgb;
+  std::mutex     _mtx;
+  bool           _capture;
+};
